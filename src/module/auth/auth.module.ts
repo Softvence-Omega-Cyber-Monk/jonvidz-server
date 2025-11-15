6adc311de325853d9b42e2ee310dd30811d92c62
Tareq Hasan <tareqhasan382@gmail.com>
2025-11-15 10:24:56 +0600

    implement Protecting routes

--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -2,15 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import {jwtConstants} from '../../common/jwt.constants';
+import { PrismaService } from '../../prisma/prisma.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
+//../../../common/jwt.constants
 @Module({
   imports: [
+    PassportModule,
     JwtModule.register({
-      secret: 'YOUR_SECRET_KEY', // <-- এটিকে একটি এনভায়রনমেন্ট ভেরিয়েবল (.env) থেকে নেওয়া উচিত
-      signOptions: { expiresIn: '1h' }, // টোকেনটি কতক্ষণ বৈধ থাকবে
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: "1d" },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    PrismaService,
+    JwtStrategy
+  ],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService) {}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    const users = await this.prisma.user.findMany();
+    return users;
   }
 
   findOne(id: number) {
