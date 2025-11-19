296744f2d17da27fded358bbe7e166b055d27ce9
Tareq Hasan <tareqhasan382@gmail.com>
2025-11-19 14:18:49 +0600

    implement progress notes crud operations

--- src/module/progress-notes/progress-notes.controller.ts
+++ src/module/progress-notes/progress-notes.controller.ts
@@ -1,34 +1,78 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
 import { ProgressNotesService } from './progress-notes.service';
 import { CreateProgressNoteDto } from './dto/create-progress-note.dto';
 import { UpdateProgressNoteDto } from './dto/update-progress-note.dto';
+import { Response } from 'express';
+import sendResponse from '../../utils/sendResponse';
 
 @Controller('progress-notes')
 export class ProgressNotesController {
   constructor(private readonly progressNotesService: ProgressNotesService) {}
 
-  @Post()
-  create(@Body() createProgressNoteDto: CreateProgressNoteDto) {
-    return this.progressNotesService.create(createProgressNoteDto);
-  }
+  // @Post()
+  // create(@Body() createProgressNoteDto: CreateProgressNoteDto) {
+  //   return this.progressNotesService.create(createProgressNoteDto);
+  // }
 
   @Get()
-  findAll() {
-    return this.progressNotesService.findAll();
+  async findAll(@Res() res: Response) {
+    const data=await this.progressNotesService.findAll();
+    if (!data || data.length === 0) {
+      return sendResponse(res, {
+        statusCode: HttpStatus.OK,
+        success: true,
+        message: 'No progress Notes data found',
+        data: [],
+      });
+    }
+    return sendResponse(res, {
+      statusCode: HttpStatus.OK,
+      success: true,
+      message: 'Progress Notes retrieve successfully.',
+      data,
+    });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.progressNotesService.findOne(+id);
+  async findOne(@Param('id') id: string, @Res() res: Response) {
+    const data = await this.progressNotesService.findOne(id);
+    return sendResponse(res, {
+      statusCode: HttpStatus.OK,
+      success: true,
+      message: 'Progress Notes retrieve successfully.',
+      data,
+    });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProgressNoteDto: UpdateProgressNoteDto) {
-    return this.progressNotesService.update(+id, updateProgressNoteDto);
+  async update(@Param('id') id: string, @Body() updateProgressNoteDto: UpdateProgressNoteDto, @Res() res: Response) {
+    const data = await this.progressNotesService.update(id, updateProgressNoteDto);
+    return sendResponse(res, {
+      statusCode: HttpStatus.OK,
+      success: true,
+      message: 'Progress Notes  successfully.',
+      data,
+    });
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.progressNotesService.remove(+id);
+  async remove(@Param('id') id: string, @Res() res: Response) {
+    const data = await this.progressNotesService.remove(id);
+    return sendResponse(res, {
+      statusCode: HttpStatus.OK,
+      success: true,
+      message: 'Progress Notes deleted successfully.',
+      data,
+    });
   }
 }
--- src/module/progress-notes/progress-notes.service.ts
+++ src/module/progress-notes/progress-notes.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateProgressNoteDto } from './dto/create-progress-note.dto';
 import { UpdateProgressNoteDto } from './dto/update-progress-note.dto';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class ProgressNotesService {
+  constructor(private readonly prisma: PrismaService) {}
   create(createProgressNoteDto: CreateProgressNoteDto) {
     return 'This action adds a new progressNote';
   }
 
-  findAll() {
-    return `This action returns all progressNotes`;
+  async findAll() {
+    return this.prisma.progressNotes.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} progressNote`;
+  async findOne(id: string) {
+    return this.prisma.progressNotes.findUnique({where: {id}});
   }
 
-  update(id: number, updateProgressNoteDto: UpdateProgressNoteDto) {
-    return `This action updates a #${id} progressNote`;
+  async update(id: string, updateProgressNoteDto: UpdateProgressNoteDto) {
+    return this.prisma.progressNotes.update({where: { id },
+      data: updateProgressNoteDto});
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} progressNote`;
+  async remove(id: string) {
+    if (!id) {
+      throw new BadRequestException('ID is required');
+    }
+    const isExists = await this.prisma.progressNotes.findUnique({
+      where: { id }
+    });
+    if (!isExists) {
+      throw new NotFoundException(`Progress Notes with ID ${id} not found`);
+    }
+    return this.prisma.progressNotes.delete({where: {id}});
   }
 }
