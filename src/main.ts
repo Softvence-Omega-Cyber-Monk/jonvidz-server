cd2b47c091f790eb12c67b38ac32a2f2ac15dd99
Tareq Hasan <tareqhasan382@gmail.com>
2025-11-28 17:20:10 +0600

    added cors-origin

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:5173','*'],
+    origin: ['http://localhost:5173','https://jonvidz.vercel.app','*'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
